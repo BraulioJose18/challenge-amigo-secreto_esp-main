d96ac5031382fefdc606d2694dee4ecd4a6b5f46
BraulioJose18 <40570187+BraulioJose18@users.noreply.github.com>
2025-03-18 17:10:03 -0500

    Add logica para programa

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const amigo = input.value.trim();
+
+    if (amigo === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+
+    if (amigos.includes(amigo)) {
+        alert("Este nombre ya ha sido añadido.");
+        return;
+    }
+    amigos.push(amigo);
+    const listaAmigos = document.getElementById('listaAmigos');
+    const li = document.createElement('li');
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+    input.value = "";
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+    const ganador = amigos[Math.floor(Math.random() * amigos.length)];
+    const resultado = document.getElementById('resultado');
+    const liResultado = document.createElement('li');
+    liResultado.textContent = `El ganador es: ${ganador}`;
+    resultado.innerHTML = '';
+    resultado.appendChild(liResultado);
+}
